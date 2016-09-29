5f11ad02611518dcac85e6e4507e0da3ef86cc93
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-09-29 13:21:25 +0200

    Project upstart

---
+++ CurrencyService/CurrencyDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CurrencyService
+{
+    public class CurrencyDTO
+    {
+        public decimal DanishKronerToEuro(decimal danishKroner)
+        {
+            decimal euro = 0;
+
+            return euro;
+        }
+    }
+}
---
+++ CurrencyService/CurrencyService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CurrencyService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CurrencyService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CurrencyService.svc or CurrencyService.svc.cs at the Solution Explorer and start debugging.
+    public class CurrencyService : ICurrencyService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
